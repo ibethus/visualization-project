2023049c7519bd0568f9f3af22cb92f85b1824b4
Axel Moriceau <contact.amoriceau@gmail.com>
2021-07-01 15:08:10 +0200

    🎉 first commit

---
+++ src/views/D3/Renderer.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h1>Future D3 data renderer</h1>
+    <div style="max-height: 600px; overflow: hidden">
+      <tree
+        style="max-height: 600px; overflow: hidden"
+        :data="tree"
+        node-text="name"
+        :duration="0"
+      >
+      </tree>
+    </div>
+  </div>
+</template>
+
+<script>
+import { tree } from "vued3tree";
+
+export default {
+  components: {
+    tree,
+  },
+  data() {
+    return {
+      tree: {
+        name: "father",
+        children: [
+          {
+            name: "son1",
+            children: [{ name: "grandson" }, { name: "grandson2" }],
+          },
+          {
+            name: "son2",
+            children: [{ name: "grandson3" }, { name: "grandson4" }],
+          },
+        ],
+      },
+    };
+  },
+};
+</script>

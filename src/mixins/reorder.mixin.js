f38f5a8926357d4cb6d83d2fb55428e37aef2bd8
Axel Moriceau <contact.amoriceau@gmail.com>
2021-07-02 14:49:33 +0200

    ✨ Use tree class to generate tree

--- src/mixins/reorder.mixin.js
+++ src/mixins/reorder.mixin.js
@@ -1,5 +1,40 @@
+import Tree from "@/classes/Tree";
+import * as definition from "@/assets/files/completedTree.json";
+// import * as definition from "@/assets/files/dummyTree.json";
+// import * as definition from "@/assets/files/sampleTree.json";
+
 export default {
   created() {
-    console.log("Test mixin");
+    console.log(definition);
+    this.definition = definition.default;
+    this.tree = this.buildTree(this.definition);
+  },
+  data() {
+    return {
+      definition: [],
+      tree: null,
+    };
+  },
+  methods: {
+    buildTree(treeDefinition) {
+      const tree = new Tree("root");
+      treeDefinition.shift();
+      treeDefinition = treeDefinition.forEach((nodes, depth) => {
+        nodes.map((node, index) => {
+          if (depth === 0) {
+            tree.createNode(node.name, index, depth);
+          } else {
+            let parents = tree.findAllNodesByDepth(depth - 1);
+            let parent = parents.find(
+              (parentNode) => node.parent == parentNode.index
+            );
+            if (parent.depth == depth - 1 && parent.index === node.parent) {
+              parent.createNode(node.name, index, depth);
+            }
+          }
+        });
+      });
+      return tree;
+    },
   },
 };

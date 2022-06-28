5eb44c5b39507054634fdfe4deb51d505b70c315
Ivan <ivan.bethus@gmail.com>
2022-06-28 09:14:33 +0200

    FIX(reorder) : Fix eslint

--- src/mixins/reorder.mixin.js
+++ src/mixins/reorder.mixin.js
@@ -1,14 +1,15 @@
+/* eslint-disable prettier/prettier */
 /* eslint-disable no-useless-escape */
 import Tree from "@/classes/Tree";
 import { BLACKLISTED_WORDS, TAGS_COLORS } from "@/helpers/constants";
 
 // Dummy dataset for presentation purpose only
-import * as definition from "@/assets/files/paysages.json";
-import * as images from "@/assets/files/formattedPaysages.json";
+//import * as definition from "@/assets/files/paysages.json";
+//import * as images from "@/assets/files/formattedPaysages.json";
 
 // Real dataset
-//import * as definition from "@/assets/files/completedTree.json";
-//import * as images from "@/assets/files/formatted_groundtruth.json";
+import * as definition from "@/assets/files/completedTree.json";
+import * as images from "@/assets/files/formatted_groundtruth.json";
 
 let id = 1;
 export default {

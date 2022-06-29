7bb21448ab96f725f1f23d5ff058888e573c08d6
Thibaut <thibaut.da-silva@etu.u-bordeaux.fr>
2022-06-29 18:52:46 +0200

    FEAT(graph) level1 data implementation working on a graph (without colorhax)

--- src/mixins/data-parser.mixin.js
+++ src/mixins/data-parser.mixin.js
@@ -1,8 +1,67 @@
-export default {
-    created() {
+import * as axios from "axios";
+import * as papa from "papaparse";
 
+export default {
+  data() {
+    return {
+      firstLevelDistanceIndexesById: Map,
+    };
+  },
+  async created() {
+    this.firstLevelDistanceIndexesById =
+      await this.getFirstLevelDistanceIndexesById();
+  },
+  methods: {
+    async getFirstLevelNodes() {
+      //var firstRow = [];
+      var indexIds = await this.getFirstLevelDistanceIndexesById();
+        return indexIds.map((indexId) => {
+          return {
+            id: indexId.id,
+            group: 1, //TODO : insérer la classe
+          };
+        });
     },
-    methods: {
-        
-    }
-}
+    async getFirstLevelDistanceIndexesById() {
+      return axios.get("/data/" + "neighbor_first.csv").then((response) => {
+        var formattedCSV = papa.parse(response.data).data;
+        formattedCSV.shift();
+        formattedCSV.pop();
+        return formattedCSV.map((row) => {
+          var id = row[0];
+          row.shift();
+          row.shift();
+          return {
+            id: id,
+            indexes: row,
+          };
+        });
+      });
+    },
+    async getFirstLevelLinks() {
+      var matchingNodesIndex = await this.getFirstLevelDistanceIndexesById();
+      var firstRowIds = [];
+      return axios.get("/data/" + "distance_first.csv").then((response) => {
+        var distanceMatrix = papa.parse(response.data).data;
+        firstRowIds = distanceMatrix[0];
+        firstRowIds.shift();
+        distanceMatrix.shift();
+        distanceMatrix.pop();
+        return distanceMatrix.flatMap((row) => {
+          const id = row[0];
+          row.shift();
+          var filteredNodesIndex = matchingNodesIndex.filter(distanceIndex => distanceIndex.id == id)[0].indexes;
+          return filteredNodesIndex.slice(0, 10).map((nodeIndex) => {
+            var distance = row[nodeIndex];
+            var linkedNodeId = firstRowIds[nodeIndex];
+            return {
+              source: id,
+              target: linkedNodeId,
+              value: Math.round(distance),
+            };
+          });
+        });
+      });
+    },
+  },
+};
--- src/views/D3/Graph.vue
+++ src/views/D3/Graph.vue
@@ -1,20 +1,23 @@
 <template>
   <div id="app">
-    <network :nodeList="nodes" :linkList="links" :linkDistance="l => l.value"></network>
+    <network v-if="!nodesLoading && !linksLoading" :nodeList="nodes" :linkList="links" :linkDistance="l => l.value"></network>
   </div>
 </template>
 
 <script>
 import Network from "vue-network-d3";
+import dataParser from "../../mixins/data-parser.mixin";
 
 export default {
   name: "Graph",
   components: {
     Network
   },
+  mixins: [dataParser],
   data() {
     return {
-      nodes: [
+      nodes: null,
+      /*[
       	{"id": "T1", "group": 1},
       	{"id": "T2", "group": 1},
         {"id": "T3", "group": 2},
@@ -23,8 +26,9 @@ export default {
         {"id": "T6", "group": 2},
         {"id": "T7", "group": 2},
         {"id": "T8", "group": 3},
-      ],
-      links: [
+      ],*/
+      links: null,
+      /*[
         {"source": "T1", "target": "T2", "value": 50},
         {"source": "T3", "target": "T4", "value": 50},
         {"source": "T1", "target": "T3", "value": 20},
@@ -33,8 +37,20 @@ export default {
         {"source": "T5", "target": "T2", "value": 20},
         {"source": "T1", "target": "T8", "value": 20},
         {"source": "T7", "target": "T6", "value": 100},
-      ]
+      ]*/
+      linksLoading: true,
+      nodesLoading: true,
     };
+  },
+  async created() {
+    this.getFirstLevelLinks().then(response => {
+      this.links = response;
+      this.nodesLoading = false;
+    });
+    this.getFirstLevelNodes().then(response => {
+      this.nodes = response;
+      this.linksLoading = false;
+    });    
   },
 };
 </script>

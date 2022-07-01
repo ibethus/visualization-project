f4f1f068bdd450b949286ccc36fa1bb83e485fb7
Ivan <ivan.bethus@gmail.com>
2022-07-01 17:28:23 +0200

    FEAT(WINDOW) : Organising windows

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,5 @@
 <template>
-  <div id="app">
-    <!-- <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/render">Render Data</router-link>
-    </div> -->
+  <div id="app" class="h-screen">
     <router-view />
   </div>
 </template>

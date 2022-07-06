dd6e0a7ec4878ac91721b240c2c8a2252a6459e0
Ivan <ivan.bethus@gmail.com>
2022-07-06 11:26:27 +0200

    FIX(GRAPH) : Delete svg icons to fix graph

--- src/components/Sidebar-component.vue
+++ src/components/Sidebar-component.vue
@@ -35,33 +35,33 @@
                         <label class="form-check-label inline-block text-gray-800 text-xs ml-2" for="tesseract">
                             Tesseract
                         </label>
-                        <div>
+                        <!-- <div>
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
                             </svg>
-                        </div>                        
+                        </div>                         -->
                     </div>
                     <div class="form-check flex items-center mt-3">
                         <input type="checkbox" value="" id="caption" @change="checkFields($event)">
                         <label class="form-check-label inline-block text-gray-800 text-xs ml-2" for="caption">
                             Caption
                         </label>
-                        <div>
+                        <!-- <div>
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z" />
                             </svg>
-                        </div>                        
+                        </div>                         -->
                     </div>
                     <div class="form-check flex items-center mt-3">
                         <input type="checkbox" value="" id="page_text" @change="checkFields($event)">
                         <label class="form-check-label inline-block text-gray-800 text-xs ml-2" for="page_text">
                             Page text
                         </label>
-                        <div>
+                        <!-- <div>
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
                             </svg>
-                        </div>                        
+                        </div>                         -->
                     </div>
                 </div>
             </div>

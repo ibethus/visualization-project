82317abdd7666dd485e1a11974ae79de7d0397ac
Thibaut <thibaut.da-silva@etu.u-bordeaux.fr>
2022-07-07 15:51:17 +0200

    FEAT(keywords) pre-filling dates working for selected classes and with keywords filters

--- src/views/D3/Graph-view.vue
+++ src/views/D3/Graph-view.vue
@@ -3,7 +3,8 @@
     <graph-image-modal :node="nodeForModal" :open="showModal" v-on:closeModal="closeGraphModal"/>
     <ul class="flex w-full px-3 py-1 shadow absolute z-20 shadow bg-gray-50 flex justify-around">
       <li class="mr-3 flex">
-        <select @change="updateData($event)" id="levels" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mt-6">
+        <select @change="updateData($event)" id="levels"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mt-6">
           <option selected>Select a level</option>
           <option value="1">Level 1</option>
           <option value="2">Level 2</option>
@@ -15,13 +16,17 @@
       <li class="mr-3">
         <div>
           <label class="text-sm" for="startDate">Starting date</label>
-          <input v-model="startingMinDate" @change="updateStartDate" id="startDate" datepicker type="date" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 datepicker-input" placeholder="Select date">
+          <input v-model="startingMinDate" id="startDate" datepicker type="date"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 datepicker-input"
+            placeholder="Select date">
         </div>
       </li>
       <li class="mr-3">
         <div>
           <label class="text-sm" for="endDate">Ending date</label>
-          <input v-model="startingMaxDate" @change="updateEndDate" id="endDate" datepicker type="date" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 datepicker-input" placeholder="Select date">
+          <input v-model="startingMaxDate" id="endDate" datepicker type="date"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 datepicker-input"
+            placeholder="Select date">
         </div>
       </li>
     </ul>
@@ -73,17 +78,26 @@ export default {
       searchResultNodes: [],
       startingMinDate: null,
       startingMaxDate: null,
+      keywordsFilteredNodes: [],
     };
   },
   async created() {
-      this.imagesData = await this.parseImagesDataJson();
-      this.rawKeywordsData = await this.parseKeywordsJson();
-      this.rawKeywordsRankingData = await this.parseKeywordsRankingJson();
-      this.imagesDatesData = this.prepareImageWithDates();
-      this.keywordsData = this.prepareKeywordsData();
-      this.keywordsRankingData = this.prepareKeywordsRankingData();
-      this.loadData(LEVELS_ENUM.One);
-      this.setDates();
+    this.imagesData = await this.parseImagesDataJson();
+    this.rawKeywordsData = await this.parseKeywordsJson();
+    this.rawKeywordsRankingData = await this.parseKeywordsRankingJson();
+    this.imagesDatesData = this.prepareImageWithDates();
+    this.keywordsData = this.prepareKeywordsData();
+    this.keywordsRankingData = this.prepareKeywordsRankingData();
+    this.loadData(LEVELS_ENUM.One);
+    this.setDates();
+  },
+  watch: {
+    startingMinDate() {
+      this.updateStartDate();
+    },
+    startingMaxDate() {
+      this.updateEndDate();
+    }
   },
   methods: {
     closeGraphModal(){
@@ -93,71 +107,122 @@ export default {
       this.nodeForModal = this.nodes.find(n => n.id == event.target.__data__.id);
       this.showModal = true;
     setDates() {
-      this.imagesDatesData.forEach(data => {
-        if(!this.startingMinDate) {
-          this.startingMinDate = data.dateMin;
-        }
-        else {
-         if (this.startingMinDate > data.dateMin) {
+      if (this.highlightedNodes.length !== 0) {
+        this.imagesDatesData.filter(data => this.highlightedNodes.includes(data.id.toString()))
+          .forEach(data => {
+            if (!this.startingMinDate) {
+              this.startingMinDate = data.dateMin;
+            }
+            else {
+              if (this.startingMinDate > data.dateMin) {
+                this.startingMinDate = data.dateMin;
+              }
+            }
+            if (!this.startingMaxDate) {
+              this.startingMaxDate = data.dateMax;
+            }
+            else {
+              if (this.startingMaxDate < data.dateMax) {
+                this.startingMaxDate = data.dateMax;
+              }
+            }
+          });
+      }
+      else {
+        this.imagesDatesData.forEach(data => {
+          if (!this.startingMinDate) {
             this.startingMinDate = data.dateMin;
           }
-        }
-        if(!this.startingMaxDate) {
-          this.startingMaxDate = data.dateMax;
-        }
-        else {
-         if (this.startingMaxDate < data.dateMax) {
+          else {
+            if (this.startingMinDate > data.dateMin) {
+              this.startingMinDate = data.dateMin;
+            }
+          }
+          if (!this.startingMaxDate) {
             this.startingMaxDate = data.dateMax;
           }
-        }
-      });
-      this.startingMinDate = new Date(this.startingMinDate).toISOString().slice(0,10);
-      this.startingMaxDate = new Date(this.startingMaxDate).toISOString().slice(0,10);
-    },
-    updateStartDate(event) {
-      this.startDateFilter = event.target.value;
-      if(this.endDateFilter) {
-        this.dateFilteredImages = this.imagesDatesData.filter(image => image.dateMin >= Date.parse(this.startDateFilter) && image.dateMax <= Date.parse(this.endDateFilter))
-        .map(image => {
-          return {
-            id: image.id.toString(),
+          else {
+            if (this.startingMaxDate < data.dateMax) {
+              this.startingMaxDate = data.dateMax;
+            }
           }
-          });
+        });
       }
-      else {
-        this.dateFilteredImages = this.imagesDatesData.filter(image => image.dateMin >= Date.parse(this.startDateFilter))
-        .map(image => {
-          return {
-            id: image.id.toString(),
-          }
-          });
-      }
-      if(this.searchResultNodes.length !== 0) {
-        this.searchResultNodes = this.searchResultNodes.filter(imageID => this.dateFilteredImages.includes(imageID));
-      }
-      else {
-        this.searchResultNodes = this.dateFilteredImages;
-      }
+
+      this.startingMinDate = new Date(this.startingMinDate).toISOString().slice(0, 10);
+      this.startingMaxDate = new Date(this.startingMaxDate).toISOString().slice(0, 10);
     },
-    updateEndDate(event) {
-      this.endDateFilter = event.target.value;
+    updateStartDate() {
+      this.startDateFilter = this.startingMinDate;
       if(this.startDateFilter) {
-        this.dateFilteredImages = this.imagesDatesData.filter(image => image.dateMin >= Date.parse(this.startDateFilter) && image.dateMax <= Date.parse(this.endDateFilter))
-        .map(image => image.id.toString());
+        if (this.endDateFilter) {
+          this.dateFilteredImages = this.imagesDatesData.filter(image => image.dateMin >= Date.parse(this.startDateFilter) && image.dateMax <= Date.parse(this.endDateFilter))
+            .map(image => { return { id: image.id.toString() } });
+        }
+        else {
+          this.dateFilteredImages = this.imagesDatesData.filter(image => image.dateMin >= Date.parse(this.startDateFilter))
+            .map(image => { return { id: image.id.toString() } });
+        }
       }
       else {
-        this.dateFilteredImages = this.imagesDatesData.filter(image => image.dateMax <= Date.parse(this.endDateFilter))
-        .map(image => image.id.toString());
+        if (this.endDateFilter) {
+          this.dateFilteredImages = this.imagesDatesData.filter(image => image.dateMax <= Date.parse(this.endDateFilter))
+            .map(image => { return { id: image.id.toString() } });
+        }
+        else {
+          this.dateFilteredImages = [];
+        }
       }
-      if(this.searchResultNodes.length !== 0) {
-        this.searchResultNodes = this.searchResultNodes.filter(imageID => this.dateFilteredImages.includes(imageID));
+
+      if (this.searchResultNodes.length !== 0) {
+        if(this.dateFilteredImages.length !== 0) {
+          this.searchResultNodes = this.searchResultNodes.filter(image => this.dateFilteredImages.some(data => data.id == image.id.toString()));
+        }
+        else {
+          this.searchResultNodes = this.keywordsFilteredNodes;
+        }
+      }
+      else {
+        this.searchResultNodes = this.dateFilteredImages;   
+      }
+    },
+    updateEndDate() {
+      this.endDateFilter = this.startingMaxDate;
+      if(this.endDateFilter) {
+        if (this.startDateFilter) {
+          this.dateFilteredImages = this.imagesDatesData.filter(image => image.dateMin >= Date.parse(this.startDateFilter) && image.dateMax <= Date.parse(this.endDateFilter))
+            .map(image => { return { id: image.id.toString() } });
+        }
+        else {
+          this.dateFilteredImages = this.imagesDatesData.filter(image => image.dateMax <= Date.parse(this.endDateFilter))
+            .map(image => { return { id: image.id.toString() } });
+        }
+      }
+      else {
+        if (this.startDateFilter) {
+          this.dateFilteredImages = this.imagesDatesData.filter(image => image.dateMin >= Date.parse(this.startDateFilter))
+            .map(image => { return { id: image.id.toString() } });
+        }
+        else {
+          this.dateFilteredImages = [];
+        }
+      }
+
+
+      if (this.searchResultNodes.length !== 0) {
+        if(this.dateFilteredImages.length !== 0) {
+          this.searchResultNodes = this.searchResultNodes.filter(image => this.dateFilteredImages.some(data => data.id == image.id.toString()));
+        }
+        else {
+          this.searchResultNodes = this.keywordsFilteredNodes;
+        }
       }
       else {
         this.searchResultNodes = this.dateFilteredImages;
       }
     },
-    updateSelectedRank(event){
-      switch(event) {
+    updateSelectedRank(event) {
+      switch (event) {
         case "1":
           this.rankFilter = KEYWORDS_RANKS_ENUM.One;
           this.searchResultNodes = this.dateFilteredImages;
@@ -176,50 +241,50 @@ export default {
           break;
       }
     },
-    updateSelectedFields(event){
-      if(event.size !== 0) {
+    updateSelectedFields(event) {
+      if (event.size !== 0) {
         this.fieldFilter = Array.from(event).map(field => {
-        switch(field) {
-          case "tesseract":
-            return KEYWORDS_FIELDS_ENUM.Tesseract;
-          case "caption":
-            return KEYWORDS_FIELDS_ENUM.Caption;
-          case "page_text":
-            return KEYWORDS_FIELDS_ENUM.PageText;
-          default:
-            return null;
-        }
-      });
+          switch (field) {
+            case "tesseract":
+              return KEYWORDS_FIELDS_ENUM.Tesseract;
+            case "caption":
+              return KEYWORDS_FIELDS_ENUM.Caption;
+            case "page_text":
+              return KEYWORDS_FIELDS_ENUM.PageText;
+            default:
+              return null;
+          }
+        });
       }
       else {
         this.fieldFilter = null;
       }
-      if(this.keywordFilter) {
-        var keywordsFilteredNodes = this.getImagesByKeywords(this.keywordFilter,this.rankFilter, this.fieldFilter);
-        if(this.dateFilteredImages.length !== 0) {
-          this.searchResultNodes = keywordsFilteredNodes.filter(node => this.dateFilteredImages.some(d => d.id == node.id.toString()));
+      if (this.keywordFilter) {
+        this.keywordsFilteredNodes = this.getImagesByKeywords(this.keywordFilter, this.rankFilter, this.fieldFilter);
+        if (this.dateFilteredImages.length !== 0) {
+          this.searchResultNodes = this.keywordsFilteredNodes.filter(node => this.dateFilteredImages.some(data => data.id == node.id.toString()));
         }
         else {
-          this.searchResultNodes = this.getImagesByKeywords(this.keywordFilter,this.rankFilter, this.fieldFilter);
+          this.searchResultNodes = this.getImagesByKeywords(this.keywordFilter, this.rankFilter, this.fieldFilter);
         }
       }
     },
-    updateSelectedKeywords(event){
+    updateSelectedKeywords(event) {
       this.keywordFilter = Array.from(event);
-      var keywordsFilteredNodes = this.getImagesByKeywords(this.keywordFilter,this.rankFilter, this.fieldFilter);
-      if(this.dateFilteredImages.length !== 0) {
-        if(this.keywordFilter.length !== 0) {
-          this.searchResultNodes = keywordsFilteredNodes.filter(node => this.dateFilteredImages.some(d => d.id == node.id.toString()));
+      this.keywordsFilteredNodes = this.getImagesByKeywords(this.keywordFilter, this.rankFilter, this.fieldFilter);
+      if (this.dateFilteredImages.length !== 0) {
+        if (this.keywordFilter.length !== 0) {
+          this.searchResultNodes = this.keywordsFilteredNodes.filter(node => this.dateFilteredImages.some(data => data.id == node.id.toString()));
         }
         else {
           this.searchResultNodes = this.dateFilteredImages;
         }
       }
       else {
-        this.searchResultNodes = this.getImagesByKeywords(this.keywordFilter,this.rankFilter, this.fieldFilter);
+        this.searchResultNodes = this.getImagesByKeywords(this.keywordFilter, this.rankFilter, this.fieldFilter);
       }
     },
-    updateData(event){
+    updateData(event) {
       this.loadData(event.target.value);
     },
     loadData(level) {
@@ -228,25 +293,25 @@ export default {
       if (levelFromParams != null) {
         levelToMap = levelFromParams;
       }
-        switch (levelToMap) {
-          case "1":
-            level = LEVELS_ENUM.One;
-            break;
-          case "2":
-            level = LEVELS_ENUM.Two;
-            break;
-          case "3":
-            level = LEVELS_ENUM.Three;
-            break;
-          case "4":
-            level = LEVELS_ENUM.Four;
-            break;
-          case "5":
-            level = LEVELS_ENUM.Five;
-            break;
-          default:
-            level = LEVELS_ENUM.One
-            break;
+      switch (levelToMap) {
+        case "1":
+          level = LEVELS_ENUM.One;
+          break;
+        case "2":
+          level = LEVELS_ENUM.Two;
+          break;
+        case "3":
+          level = LEVELS_ENUM.Three;
+          break;
+        case "4":
+          level = LEVELS_ENUM.Four;
+          break;
+        case "5":
+          level = LEVELS_ENUM.Five;
+          break;
+        default:
+          level = LEVELS_ENUM.One
+          break;
       }
       this.imageIdsFromParams = this.getClassImageIds(this.$route.query.nodeClass, level);
       if (this.imageIdsFromParams != null) {
@@ -274,6 +339,7 @@ export default {
 body {
   margin: 0;
 }
+
 .filters {
   position: absolute;
   left: 0;
@@ -281,6 +347,7 @@ body {
   margin-left: auto;
   margin-right: auto;
 }
+
 li {
   cursor: pointer;
 }
